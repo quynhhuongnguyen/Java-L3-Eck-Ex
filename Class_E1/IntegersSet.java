51fd8489c98ca4de72814acc7cb952869c14cc83
Hương Quỳnh <huongquynh@mbp-de-huong.home>
2018-03-17 12:51:36 +0100

    Exercice Interget Set[M]

---
+++ Class_E1/IntegersSet.java
@@ -0,0 +1,21 @@
+package Class_E1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IntegersSet {
+	public static void main(String[] args) {
+		 ArrayList<Integer> IntSet = new ArrayList<Integer>();
+		 Scanner scan = new Scanner(System.in);
+
+		 System.out.println("Please enter numbers. Enter 0.");
+		 while (scan.hasNextLine()) {
+		      int num = Integer.parseInt(scan.next());
+		      if(num == 0) break;
+		      IntSet.add(num);  
+		 }
+		 System.out.println(IntSet);
+	 }
+}
+
